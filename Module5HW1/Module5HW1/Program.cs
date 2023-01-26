43e9daa64317b9a90c179bdad019b8d9c126b011
Niffe <petrovnik01@ukr.net>
2023-01-26 15:24:28 +0200

    Add empty project

---
+++ Module5HW1/Module5HW1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module5HW1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
